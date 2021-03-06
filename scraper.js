ee66c596e129742d69eff1825e8e01e3933d1dca
Luu-Quan Thom Huynh <luu-quan.thom.huynh@sap.com>
2021-03-06 15:59:06 +0100

    create local html file of scraped target website (works for single website)

--- scraper.js
+++ scraper.js
@@ -21,20 +21,17 @@ async function crawl(searchEngineURL, query){
     await page.click('label[for=sb_form_go]');
     
     //bing: close edge ad popup
-    //TODO use inner text with "Maybe Later"
     await page.waitForSelector('span[id=bnp_hfly_cta2]'); // this changes -> A/B testing from bing
     await page.click('span[id=bnp_hfly_cta2]');
 
-    await page.screenshot({ path: `SE_BING_1_2021400009(page1).png`, fullPage: true }); 
-    // TODO make file out of source code without multimedia
-    const html = await page.content();
-
-    //get all entries from first two pages
+    
+    //bing: get all entries from first two pages
     const results1 = await page.$$eval('.b_algo h2 a', results => results.map((result,index) => ({
         rank: index + 1,
         title: result.textContent,
         url: result.href
     })))
+    await page.screenshot({ path: `SE_BING_1_2021400009(page1).png`, fullPage: true }); 
 
     await page.click('.b_pag li:last-child a');
     await page.waitForSelector('.b_algo h2 a');
@@ -52,6 +49,15 @@ async function crawl(searchEngineURL, query){
     var fs = require('fs');
     const jsonString = JSON.stringify(results);
     fs.writeFile('SE_BING_1_2021400009.json', jsonString, 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+
+    //write html page to file
+    const html = await page.content();
+
+    fs.writeFile('pageContent.html', html, function (err) {
         if (err) {
             return console.log(err);
         }
